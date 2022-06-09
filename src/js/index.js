35f4d7c95cc86934249ad2c514d3516b089b4cd7
RenatoAnjo <renatinho.anji@gmail.com>
2022-06-08 22:09:07 -0300

    Continuando exercicios de algoritmos js

--- src/js/index.js
+++ src/js/index.js
@@ -71,3 +71,125 @@ funcao, que retorna a concatenacao dos dois parametros que ela recebe */
 function concatenar(texto1, texto2) {
   return texto1 + texto2
 }
+
+/* Somar Números
+Implemente a função ao lado, que soma dois números e retorna o texto A soma deu: x, sendo x o resultado da soma */
+
+function somar(num1, num2) {
+  let resultado = num1 + num2
+  return 'A soma deu: ' + resultado
+}
+
+/* É Maior de Idade?
+Implemente a função ao lado, que retorna Maior de idade caso a idade passada seja maior que 17 ou Menor de idade caso contrário. */
+
+function maiorDeIdade(idade) {
+  if (idade > 17) {
+    return 'Maior de idade'
+  } else {
+    return 'Menor de idade'
+  }
+}
+
+/* Foi Aprovado?
+Implemente a função ao lado, que retorna Aprovado caso a nota passada seja maior ou igual a 7, Prova Final caso seja maior ou igual a 5 porém menor que 7 ou Reprovado caso contrário. */
+
+function situacao(nota) {
+  if (nota >= 7) {
+    return 'Aprovado'
+  } else if (nota >= 5) {
+    return 'Prova Final'
+  } else {
+    return 'Reprovado'
+  }
+}
+
+/* Fui Aprovado com Média
+Implemente a função ao lado, que recebe 3 notas e retorna as strings sim ou não, indicando se a média simples entre as 3 notas (somar e dividir por 3) é maior ou igual a 7. */
+
+function aprovado(nota1, nota2, nota3) {
+  let resultado = (nota1 + nota2 + nota3) / 3
+  if (resultado >= 7) {
+    return 'sim'
+  } else {
+    return 'não'
+  }
+}
+
+/* 
+Cade meu Desconto?
+Implemente a função ao lado, que recebe um preço e um booleano indicando se já está com desconto ou não. Se o preço for maior que 100 e não estiver com desconto, a função deve retornar Quero pechinchar. Caso contrário, deve retornar Negócio fechado */
+
+function pecoDesconto(preco, estaComDesconto) {
+  if (preco > 100 && estaComDesconto == false) {
+    return 'Quero pechinchar'
+  } else {
+    return 'Negócio fechado'
+  }
+}
+
+/* Verifica faltas
+Implemente a função ao lado, que recebe uma nota e um número de faltas e retorna Aprovado caso a média seja maior ou igual a 7 e o número de faltas menor que 10, ou Reprovado caso contrário. */
+
+function aprovado(nota, faltas) {
+  if (nota >= 7 && faltas < 10) {
+    return 'Aprovado'
+  } else {
+    return 'Reprovado'
+  }
+}
+
+/* Tem Negativo?
+Implemente a função ao lado, que recebe 3 números e retorna Tem negativo! caso pelo menos 1 deles seja menor que 0. Caso contrário, ela deve retornar Tudo positivo! */
+
+function tudoPositivo(num1, num2, num3) {
+  if (num1 < 0 || num2 < 0 || num3 < 0) {
+    return 'Tem negativo!'
+  } else {
+    return 'Tudo positivo!'
+  }
+}
+
+/* RepetidoRepetido
+'Utilizando loops, implemente a função ao lado que recebe um texto e retorna uma string com o texto repetido 10 vezes. */
+
+function vezes10(texto) {
+  let resultado = ''
+
+  for (let i = 0; i < 10; i++) {
+    resultado = resultado + texto
+  }
+  return resultado
+}
+
+/* 
+Multiplica Aí
+Utilizando loops, implemente a função ao lado que recebe um texto e um número de repetições. Ela deve retornar uma string com o texto repetido n vezes, sendo n o número de repetições. */
+
+function multiplica(texto, repeticoes) {
+  let resultado = ''
+
+  for (let i = 0; i < repeticoes; i++) {
+    resultado += texto
+  }
+
+  return resultado
+}
+
+/* Lista de Nomes
+Implemente a função ao lado que recebe 3 nomes e retorna esses nomes em uma array. */
+
+function listar(nome1, nome2, nome3) {
+  return [nome1, nome2, nome3]
+}
+
+/* Dobrar a meta
+Implemente a função ao lado que recebe uma array e retorna a soma de todos os seus números multiplicados por 2. */
+
+function dobraASoma(lista) {
+  let soma = 0
+  for (let i = 0; i < lista.length; i++) {
+    soma = soma + lista[i]
+  }
+  return soma * 2
+}
