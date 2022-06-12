68ab58c7add80d29703edac7ac141d3a52ead8b6
RenatoAnjo <renatinho.anji@gmail.com>
2022-06-12 19:01:27 -0300

    Continuacao exercicios

--- src/js/index.js
+++ src/js/index.js
@@ -401,3 +401,65 @@ function somaAteMeta(inicio, meta) {
 
   return numeros
 }
+
+/* SubArray
+Implemente a função ao lado que recebe dois arrays como parâmetro e deve retornar true caso o 2o array seja uma subsequência do 1o array e false caso contrário
+
+Obs: uma subsequência é um conjunto de números que está contido dentro de uma sequência maior de números e na mesma ordem. Por isso, “1,3,5” é uma subsequência de “1,2,3,4,5”, pois todos os números do 1o conjunto aparecem no 2o conjunto e na mesma ordem (o 1 vem antes do 3 e o 3 vem antes do 5 na sequencia original).
+
+Exemplo: se forem passados os arrays “[1,4,3,5]” e “[1,2,4]”, a função deve retornar false, pois o número “2” não está presente no 1o array
+
+Exemplo: se forem passados os arrays “[1,4,3,5]” e “[1,3,4]”, a função deve retornar false, pois, embora todos os valores do 2o array estejam presente no 1o, a ordem não foi respeitada (o “4” deveria vir antes do “3”). */
+
+function subArray(array, subarray) {
+  let indiceMenor = 0
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === subarray[indiceMenor]) {
+      indiceMenor++
+
+      if (indiceMenor === subarray.length) break
+    }
+  }
+
+  return indiceMenor === subarray.length
+}
+
+/* Letras Repetidas
+Implemente a função ao lado que recebe uma string como parâmetro e retorna um array contendo as letras repetidas na string passada
+
+Obs:
+
+Se não tiver repetição, retorne o array “[]”
+
+Caso mais de uma letra esteja repetida na string, ordene o array de retorno na ordem que as letras aparecem na string originial
+
+Serão passadas strings com no máximo 2 repetições de cada letra, ou seja, não se preocupe com palavras como “carreira”, onde o “r” aparece 3 vezes
+
+Exemplo: se for passado “ovo”, a função deve retornar “[o]”
+
+Exemplo: se for passado “passagem”, a função deve retornar “[s,a]”
+
+Exemplo: se for passado “uva”, a função deve retornar “[]” */
+
+function letrasRepetidas(string) {
+  let letras = []
+  let acumuladoQuantidade = []
+
+  for (let i = 0; i < string.length; i++) {
+    let letra = string[i]
+    let limite = i
+    let quantidade = 0
+
+    for (let j = 0; j < limite; j++) {
+      if (string[j] === letra) quantidade++
+    }
+    acumuladoQuantidade.push(quantidade)
+  }
+
+  for (let i = 0; i < acumuladoQuantidade.length; i++) {
+    if (acumuladoQuantidade[i] === 1) letras.push(string[i])
+  }
+
+  return letras
+}
