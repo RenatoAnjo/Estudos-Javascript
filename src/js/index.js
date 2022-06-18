56977e288b0b1c5e432cdb48a6eca4a96fd5915f
RenatoAnjo <renatinho.anji@gmail.com>
2022-06-18 20:06:10 -0300

    Exercicios js

--- src/js/index.js
+++ src/js/index.js
@@ -820,3 +820,82 @@ function retornarPositivos(ar) {
 }
 var ar = [-5, 10, -3, 12, -9, 5, 90, 0, 1]
 var ar2 = retornarPositivos(ar)
+
+// Localizar o maior valor dentro de um array de números
+function LocalizarMaior(ar) {
+  var max = ar[0]
+  for (var i = 0; i < ar.length; i++) {
+    if (ar[i] > max) {
+      max = ar[i]
+    }
+  }
+  return max
+}
+var ar = [-5, 10, -3, 12, -9, 5, 90, 0, 1]
+var max = LocalizarMaior(ar)
+console.log('Maior número: ', max)
+
+// Calcular a soma de todos os dígitos de um número inteiro positivo
+function somarDigitos(n) {
+  var s = n.toString()
+  var soma = 0
+  for (var char of s) {
+    var digito = parseInt(char)
+    soma += digito
+  }
+  return soma
+}
+var resultado = somarDigitos(1235231)
+console.log('Soma de todos os dígitos: ', resultado)
+
+/* Imprimir os 10 primeiros números da sequência de Fibonacci sem usar recursividade
+A sequência de Fibonacci é composta por uma sucessão de números descrita pelo famoso
+matemático italiano Leonardo de Pisa (1170-1250), mais conhecido como Fibonacci, no final do
+século 12. O matemático percebeu uma regularidade matemática a partir de um problema
+criado por ele mesmo. Além disso, quando esses números são transformados em quadrados e
+dispostos de maneira geométrica, é possível traçar uma espiral, que curiosamente também
+pode ser vista em muitos fenômenos naturais.
+A fórmula usada para calcular a sequência de Fibonacci é a seguinte: Fn = Fn - 1 + Fn - 2 
+(Fn = Fator numeral)
+Neste caso, repetimos o número 1 e somamos os dois, ou seja, 1 + 1 = 2. Em seguida, somamos
+o resultado com o número antecessor, ou seja, 2 + 1 = 3, e assim continuamente, em uma
+sequência infinita */
+var f0 = 0
+console.log(f0)
+var f1 = 1
+console.log(f1)
+for (var i = 2; i < 10; i++) {
+  var fi = f1 + f0
+  console.log(fi)
+  f0 = f1
+  f1 = fi
+}
+
+/* Criar uma função para imprimir os 10 primeiros números da sequência de Fibonacci usando
+recursividade */
+function localizarFibonacci(n) {
+  if (n == 0) return 0
+  if (n == 1) return 1
+  return localizarFibonacci(n - 1) + localizarFibonacci(n - 2)
+}
+var n = localizarFibonacci(10)
+console.log(n)
+
+/* Criar uma função que retorno um valor booleano informando se o número enviado como
+parâmetro é um número primo.*/
+function numeroPrimo(n) {
+  if (n < 2) return false
+  if (n == 2) return true
+  var maxDiv = Math.sqrt(n)
+  for (var i = 2; i <= maxDiv; i++) {
+    if (n % i == 0) {
+      return false
+    }
+  }
+  return true
+}
+console.log(2, ' é um número primo? ', numeroPrimo(2))
+console.log(3, ' é um número primo? ', numeroPrimo(3))
+console.log(4, ' é um número primo? ', numeroPrimo(4))
+console.log(5, ' é um número primo? ', numeroPrimo(5))
+console.log(9, ' é um número primo? ', numeroPrimo(9))
