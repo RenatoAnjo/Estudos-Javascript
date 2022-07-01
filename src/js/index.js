8cd8b26b7529fbdfe60e8f81ba601425f0e5019e
RenatoAnjo <renatinho.anji@gmail.com>
2022-06-30 23:02:52 -0300

    Exercicios js

--- src/js/index.js
+++ src/js/index.js
@@ -1932,3 +1932,40 @@ try {
 } catch (e) {
   console.log(e)
 }
+
+//Exercício de lógica de programação para copiar um vetor
+
+var a = [1, 2]
+console.log(a); // [ 1, 2 ]
+
+var b = a
+console.log(b); // [ 1, 2 ]
+
+a.push(3)
+console.log(a); // [ 1, 2, 3 ]
+console.log(b); // [ 1, 2, 3 ]
+
+var assert = require('assert');
+
+//
+// Seu código
+//
+var clonar = function(data){
+    var clone = [];
+    for(var i = 0; i < data.length; i++){
+        clone.push(data[i]);
+    }
+    return clone;
+}
+//
+// Seu teste
+//
+try {
+
+    assert.deepEqual([10, 20, 30, 0], clonar([10, 20, 30, 0]));
+
+} catch(e) {
+    console.log(e);
+}
+
+//Para comparar dois arrays precisamos usar deepEqual() ao invés de equal().
