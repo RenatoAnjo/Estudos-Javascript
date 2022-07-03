fdfdb377e6b93185adf84eea37047c1317ddf961
RenatoAnjo <renatinho.anji@gmail.com>
2022-07-02 21:28:09 -0300

    Exercicios js vetor

--- src/js/index.js
+++ src/js/index.js
@@ -2020,3 +2020,61 @@ try {
 } catch (e) {
   console.log(e)
 }
+
+//Valores repetidos em vetor
+
+var assert = require('assert')
+
+var haDuplicados = function (arr) {
+  // aqui entra a sua lógica
+}
+
+//
+// Seu teste
+//
+try {
+  assert.equal(true, haDuplicados([100, 200, 300, 300, 500]))
+  assert.equal(false, haDuplicados([100, 200, 300, 400, 500]))
+} catch (e) {
+  console.log(e)
+}
+
+var assert = require('assert')
+
+var haDuplicados = function (arr) {
+  var i
+  for (i = 0; i < arr.length; i++) {
+    if (arr.indexOf(arr[i]) != i) {
+      return true
+    }
+  }
+  return false
+}
+
+//
+// Seu teste
+//
+try {
+  assert.equal(true, haDuplicados([100, 200, 300, 300, 500]))
+  assert.equal(false, haDuplicados([100, 200, 300, 400, 500]))
+} catch (e) {
+  console.log(e)
+}
+
+var haDuplicados = function (arr) {
+  var i
+  //
+  // Vamos percorrer o array e...
+  //
+  for (i = 0; i < arr.length; i++) {
+    // ..se o índice do valor atual "indexOf(arr[i])"
+    // for diferente do índice corrente "i"
+    // retornamos "true"
+    if (arr.indexOf(arr[i]) != i) {
+      return true
+    }
+  }
+  // Se não houver repetido
+  // retornamos false
+  return false
+}
