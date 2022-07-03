1ee1a15827b6155b43b036e5596c23ced1aa5124
RenatoAnjo <renatinho.anji@gmail.com>
2022-07-03 19:27:17 -0300

    Exercicios string

--- src/js/index.js
+++ src/js/index.js
@@ -2078,3 +2078,43 @@ var haDuplicados = function (arr) {
   // retornamos false
   return false
 }
+
+//Contar vogais string exercicio
+
+var assert = require('assert')
+
+var contarVogais = function (palavra) {}
+
+//
+// Seu teste
+//
+try {
+  assert.equal(6, contarVogais('abecedario'))
+} catch (e) {
+  console.log(e)
+}
+
+var assert = require('assert')
+
+var contarVogais = function (palavra) {
+  var totalVogal = 0
+  var vogais = ['a', 'e', 'i', 'o', 'u']
+  var i
+
+  for (i = 0; i < palavra.length; i++) {
+    if (vogais.indexOf(palavra[i]) != -1) {
+      totalVogal++
+    }
+  }
+
+  return totalVogal
+}
+
+//
+// Seu teste
+//
+try {
+  assert.equal(6, contarVogais('abecedario'))
+} catch (e) {
+  console.log(e)
+}
