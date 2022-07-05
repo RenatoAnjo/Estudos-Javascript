2cf0344489ec4549eabad1d962500c78dc5b4eb6
RenatoAnjo <renatinho.anji@gmail.com>
2022-07-04 22:53:00 -0300

    Exercicios js OOP

--- src/js/index.js
+++ src/js/index.js
@@ -2118,3 +2118,52 @@ try {
 } catch (e) {
   console.log(e)
 }
+
+//O dobro de um número qualquer
+
+var assert = require('assert')
+
+//
+// Está é nossa classe
+//
+function Calc() {
+  //
+  // Este é o método da classe
+  //
+  this.dobro = function () {
+    //
+    // aqui entra a sua lógica
+    //
+  }
+}
+
+try {
+  var calc = new Calc()
+  assert.equal(10, calc.dobro(5))
+} catch (e) {
+  console.log(e)
+}
+
+var assert = require('assert')
+
+//
+// Está é nossa classe
+//
+function Calc() {
+  //
+  // Este é o método da classe
+  //
+  this.dobro = function (num) {
+    //
+    // aqui entra a sua lógica
+    //
+    return num * 2
+  }
+}
+
+try {
+  var calc = new Calc()
+  assert.equal(10, calc.dobro(5))
+} catch (e) {
+  console.log(e)
+}
