aaa2492341a50e3f3704b7bf3927596a394f1b11
Renato Anjo da Silva <103430282+RenatoAnjo@users.noreply.github.com>
2022-08-31 20:33:48 -0300

    6 remocao de arrays

--- src/js/index.js
+++ src/js/index.js
@@ -2671,3 +2671,71 @@ while(adicionarMais != "não"){
     }
 }
 alert(`Lista de compras:\n  Frutas: ${frutas}\n  Laticínios: ${laticinios}\n  Doces: ${doces}\n  Congelados: ${congelados}`);
+
+
+/* Lógica JS 6/7: 👩🏽‍💻 Remoção de Arrays */
+
+
+let frutas = [];
+let laticinios = [];
+let doces = [];
+let congelados = [];
+let comida = "";
+let categoria = "";
+let remover = "";
+
+let adicionarMais = "sim";  // valor inicial como "sim", porque a primeira vez sempre vai entrar no while
+while(adicionarMais != "não"){
+    if(frutas.length === 0 && laticinios.length === 0 && doces.length === 0 && congelados.length === 0){
+        adicionarMais = prompt("Você deseja adicionar uma comida na lista de compras? Responda 'sim' ou 'não'.");
+    } else {
+        adicionarMais = prompt("Você deseja adicionar uma comida na lista de compras? Responda 'sim', 'não' ou 'remover'.");
+    }
+	
+    while (adicionarMais != "sim" && adicionarMais != "não" && adicionarMais != "remover") {  //enquanto o texto lido for diferente de "sim", "não" e "remover", exibir que não foi reconhecido e perguntar novamente
+	alert(`Operação não reconhecida!`);
+        adicionarMais = prompt("Você deseja adicionar uma comida na lista de compras? Responda 'sim' ou 'não'.");
+    }
+	
+    if (adicionarMais === "não"){  //se o texto lido for "não", sair do while
+	break;
+    }
+	
+    if(adicionarMais === "sim"){
+        comida = prompt("Qual comida você deseja inserir?");
+        categoria = prompt("Em qual categoria essa comida se encaixa: 'frutas', 'laticínios', 'doces' ou 'congelados'?");
+        if(categoria === 'frutas'){
+            frutas.push(comida);
+        } else if (categoria === 'laticínios'){
+            laticinios.push(comida);
+        } else if (categoria === 'doces'){
+            doces.push(comida);
+        } else if (categoria === 'congelados'){
+            congelados.push(comida);
+        } else {
+            alert("Essa categoria não foi pré-definida.")
+        }
+    } else if(adicionarMais === "remover"){
+	if(frutas.length === 0 && laticinios.length === 0 && doces.length === 0 && congelados.length === 0){  //se a lista estiver vazia (tratamento de bug, caso a pessoa digite "remover" mesmo quando forem exibidas apenas as opções "sim" e "não"
+		alert(`A lista está vazia!`);
+	} else {  //se a lista não estiver vazia
+		remover = prompt(`Lista de compras:\n  Frutas: ${frutas}\n  Laticínios: ${laticinios}\n  Doces: ${doces}\n  Congelados: ${congelados}\n\nQual produto você deseja remover?`);
+		if(frutas.indexOf(remover) != -1){
+			frutas.splice(frutas.indexOf(remover), 1);
+			alert(`O item ${remover} foi removido com sucesso!`)
+		} else if(laticinios.indexOf(remover) != -1){
+			laticinios.splice(laticinios.indexOf(remover), 1);
+			alert(`O item ${remover} foi removido com sucesso!`)
+		} else if (doces.indexOf(remover) != -1){
+			doces.splice(doces.indexOf(remover), 1);
+			alert(`O item ${remover} foi removido com sucesso!`)
+		} else if (congelados.indexOf(remover) != -1){
+			congelados.splice(congelados.indexOf(remover), 1);
+			alert(`O item ${remover} foi removido com sucesso!`)
+		} else {
+			alert(`Não foi possível encontrar o item dentro da lista!`)
+		}
+	}
+    }
+}
+alert(`Lista de compras:\n  Frutas: ${frutas}\n  Laticínios: ${laticinios}\n  Doces: ${doces}\n  Congelados: ${congelados}`);
