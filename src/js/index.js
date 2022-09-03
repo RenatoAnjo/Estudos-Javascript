4c2122cd5e7ca5e22005fd76483b9a8e1c1c277e
Renato Anjo da Silva <103430282+RenatoAnjo@users.noreply.github.com>
2022-09-03 18:56:40 -0300

    estrutura de repeticao, while, do while e for

--- src/js/index.js
+++ src/js/index.js
@@ -2941,3 +2941,32 @@ switch (cargo) {
     default:
         salario *= 1.05;
 }
+
+
+/* while e do while */
+//Podemos utilizar a estrutura de repetição while caso seja necessário repetir um bloco de comandos por N vezes.
+
+let i = 0;
+while(i < 11){
+    console.log('5 x ' + i + ' = ' + 5*i);
+    i++;
+}
+
+/* do-while tem o mesmo conceito que o while com uma única diferença, a condição é verificada após os comandos do bloco serem executados, 
+ou seja, mesmo que a condição seja falsa, é garantia que o bloco será executado ao menos uma vez. */
+
+
+let contador = 0;
+
+do{
+	console.log("O contador vale: " + contador);
+	contador++;
+}while(contador == 1)
+	
+
+/* for */ 
+
+for(let i = 0; i < 11; i++){
+    console.log("5 x " + i + " = " + 5*i);
+}
+
