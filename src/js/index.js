0723b7ef8fcf9c19b86ad142d0c5600c6c84d1b0
Renato Anjo da Silva <103430282+RenatoAnjo@users.noreply.github.com>
2022-09-02 23:06:46 -0300

    estrutura repeticao switch case

--- src/js/index.js
+++ src/js/index.js
@@ -2793,3 +2793,151 @@ do {  //usando o 'do...while', já que a primeira vez sempre vamos entrar
     }
 } while(operacao === "soma" || operacao === "subtração" || operacao === "multiplicação" || operacao === "divisão")
 alert(`Até a próxima!`);
+
+/* switch case */ 
+
+let signo = 'Leão';
+
+switch (signo) {
+  case 'Áries':
+    console.log("De 21 março a 20 abril");
+    break;
+	case 'Touro':
+    console.log("de 21 abril a 20 maio");
+    break;
+  case 'Gêmeos':
+    console.log("de 21 maio a 20 junho");
+    break;
+  case 'Câncer':
+    console.log("de 21 junho a 22 julho");
+    break;
+  case 'Leão':
+    console.log("de 23 julho a 22 agosto");
+    break;
+  case 'Virgem':
+    console.log("de 23 agosto a 22 setembro");
+    break;
+  default:
+  	console.log("Signo não registrado");
+    break;
+}
+
+
+// Valor impresso no console: "Usuário logado"
+autenticado = true;
+
+switch (autenticado) {
+    case true:
+        console.log("Usuário logado");
+        break;
+    case false:
+        console.log("Usuário não autenticado");
+}
+
+// Valor impresso no console: "Bem-vinda!"
+sexo = "feminino";
+
+switch (sexo) {
+    case "feminino":
+        console.log("Bem-vinda!");
+        break;
+    case false:
+        console.log("Bem-vindo!");
+}
+
+// Valor impresso no console: "Outono"
+mes = "Maio";
+
+switch (mes) {
+    case "Janeiro":
+    case "Fevereiro":
+    case "Março":
+        console.log("Verão!");
+        break;
+    case "Abril":
+    case "Maio":
+    case "Junho":
+        console.log("Outono!");
+        break;
+    case "Julho":
+    case "Agosto":
+    case "Setembro":
+        console.log("Inverno!");
+        break;
+    case "Outubro":
+    case "Novembro":
+    case "Dezembro":
+        console.log("Primavera!");
+}
+
+// Valor impresso no console: "Abril"
+mes = 4;
+nomeMes = "";
+
+switch (mes) {
+    case 1:
+        nomeMes = "Janeiro";
+        break;
+    case 2:
+        nomeMes = "Fevereiro";
+        break;
+    case 3:
+        nomeMes = "Março";
+        break;
+    case 4:
+        nomeMes = "Abril";
+        break;
+    case 5:
+        nomeMes = "Maio";
+        break;
+    case 6:
+        nomeMes = "Junho";
+        break;
+    case 7:
+        nomeMes = "Julho";
+        break;
+    case 8:
+        nomeMes = "Agosto";
+        break;
+    case 9:
+        nomeMes = "Setembro";
+        break;
+    case 10:
+        nomeMes = "Outubro";
+        break;
+    case 11:
+        nomeMes = "Novembro";
+        break;
+    case 12:
+        nomeMes = "Dezembro";
+        break;
+    default:
+        nomeMes = "Mês inexistente";
+}
+console.log(nomeMes);
+
+// Usuário redirecionado para "homejovem.html"
+tipoUsuario = "Adolescente";
+
+switch (tipoUsuario) {
+    case "Adolescente":
+        window.location.href = "homejovem.html";
+        break;
+    case "Adulto":
+        window.location.href = "home.html";
+}
+
+// Valor atribuído à variável salario: 2300
+cargo = "gerente";
+salario = 2000;
+
+switch (cargo) {
+    case "gerente":
+        salario *= 1.15;
+        break;
+    case "supervisor":
+        salario *= 1.10;
+        break;
+    default:
+        salario *= 1.05;
+}
