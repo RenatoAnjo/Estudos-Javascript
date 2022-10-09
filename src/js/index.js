45a7b778da6120afa8d376f01dccb6014ebaf88d
Renato Anjo da Silva <103430282+RenatoAnjo@users.noreply.github.com>
2022-10-08 23:49:36 -0300

    Eventos com javascript

--- src/js/index.js
+++ src/js/index.js
@@ -3313,3 +3313,15 @@ eventos de teclado (KeyboardEvent): keydown, keypress, keyup
 eventos de formulário: focus, blur, change, submit
 eventos de janela: scroll, resize, hashchange, load, unload
 */
+
+/*
+onBlur	remove o foco do elemento
+onChange	muda o valor do elemento
+onClick	o elemento é clicado pelo usuário
+onFocus	o elemento é focado
+onKeyPress	o usuário pressiona uma tecla sobre o elemento
+onLoad	carrega o elemento por completo
+onMouseOver	define ação quando o usuário passa o mouse sobre o elemento
+onMouseOut	define ação quando o usuário retira o mouse sobre o elemento
+onSubmit	define ação ao enviar um formulário
+*/
