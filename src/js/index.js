ffcbf2b85cb9291b9d60a4ab9c8a5626167f11b6
Renato Anjo da Silva <103430282+RenatoAnjo@users.noreply.github.com>
2022-11-05 23:53:16 -0300

    While loop

--- src/js/index.js
+++ src/js/index.js
@@ -3766,3 +3766,11 @@ for (let i =0 ; i< names.length ; i++) {
 }	
 	
 	
+// while loop
+const names = ['Jobs', 'Hopper', 'Turing']
+let i = 0
+
+while ( i < names.length) {
+	console.log(names[i])
+	i++
+}	
