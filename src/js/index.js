5ee7c5d78c6b16bb70ec82d94f0e373bb2f49c47
Renato Anjo da Silva <103430282+RenatoAnjo@users.noreply.github.com>
2022-11-10 22:57:43 -0300

    dias da semana switch case

--- src/js/index.js
+++ src/js/index.js
@@ -3845,3 +3845,30 @@ switch (grade)
 	default:
 		console.log('nota invalida')
 }			
+
+/* exemplo switch case */
+var agora = new Date()
+var diaSem = agora.getDay()
+switch(diaSem){
+  case 0:
+    console.log(‘Domingo’)
+    break
+  
+  case 1:
+    console.log(‘Segunda’)
+    break
+case 2:
+    console.log(‘Terça’)
+    break
+case 3:
+    console.log(‘Quarta’);
+    break
+case 4:
+    console.log(‘Quinta’);
+    break
+case 5:
+    console.log(‘Sexta’);
+    break
+default:
+    console.log(‘Sábado’);
+}
