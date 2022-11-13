5bb6c0f344f7eee128dee266c9c1c56934819045
Renato Anjo da Silva <103430282+RenatoAnjo@users.noreply.github.com>
2022-11-12 23:15:01 -0300

    exercicio de fixacao while

--- src/js/index.js
+++ src/js/index.js
@@ -3898,3 +3898,19 @@ switch(chosenOption) {
         convertedDistance = "Conversão fora do escopo"
 }
 alert("Distância em Anos-luz: " + distanceInLY + "\n" + chosenUnity + ": " + convertedDistance)
+
+/* exercicio de fixacao while*/
+
+let warpCount = 0
+let chosenOption = ""
+
+let spaceship = prompt("Digite o nome da nave")
+
+chosenOption = prompt("Deseja entrar em dobra espacial?\n1- Sim\n2- Não")
+
+while(chosenOption == "1") {
+    warpCount += 1
+    chosenOption = prompt("Deseja realizar a próxima dobra?\n1- Sim\n2- Não")
+}
+
+alert("Nave: " + spaceship + "\nQuantidade de dobras: " + warpCount)
