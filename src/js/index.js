6ed1926915763d4edd9e2db8893a6fb41221888a
Renato Anjo da Silva <103430282+RenatoAnjo@users.noreply.github.com>
2022-11-22 22:17:12 -0300

    calculadora 4 operacoes

--- src/js/index.js
+++ src/js/index.js
@@ -4176,3 +4176,24 @@ function mostrarTabuada(){
                 resultado.appendChild(div);
             }
 }
+
+/* calculadora das 4 Operacoes*/
+	
+const entrada1 = prompt("Informe o primeiro número:")
+const entrada2 = prompt("Informe o segundo número:")
+
+const x = parseFloat(entrada1)
+const y = parseFloat(entrada2)
+
+const soma = x + y
+const subtracao = x - y
+const multiplicacao = x * y
+const divisao = x / y
+
+alert(
+  "Resultados:\n" +
+  "\nSoma: " + soma +
+  "\nSubtração: " + subtracao +
+  "\nMultiplicação: " + multiplicacao +
+  "\nDivisão: " + divisao
+)
