70cd3236f1d53b0fda973eed9b31a77cc7401a1e
Renato Anjo da Silva <103430282+RenatoAnjo@users.noreply.github.com>
2022-11-25 23:58:47 -0300

    exercicio visitando varias cidades

--- src/js/index.js
+++ src/js/index.js
@@ -4227,3 +4227,20 @@ function calcular()
     }
 }
 	
+/* exercicio visitando cidades*/
+	
+const turista = prompt("E aí, turista! Qual é o seu nome?")
+let cidades = ""
+let contagem = 0
+let continuar = prompt("Você visitou alguma cidade? (Sim/Não)")
+while (continuar === "Sim") {
+  let cidade = prompt("Qual é o nome da cidade visitada?")
+  cidades += " - " + cidade + "\n"
+  contagem++
+  continuar = prompt("Você visitou alguma outra cidade? (Sim/Não)")
+}
+alert(
+  "Turista: " + turista +
+  "\nQuantidade de cidades visitadas: " + contagem +
+  "\nCidades visitadas:\n" + cidades
+)	
