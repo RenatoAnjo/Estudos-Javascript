9ca75b06a47cc7c2e1b41e9edcfbfde490c7e96a
Renato Anjo da Silva <103430282+RenatoAnjo@users.noreply.github.com>
2022-12-02 23:52:53 -0300

    Continuacao trabalhando com arrays

--- src/js/index.js
+++ src/js/index.js
@@ -4425,5 +4425,42 @@ elementoRemovido = arr.shift("Boromir")
 console.log(arr)
 console.log(elementoRemovido)
 	
+/* Pesquisando por um elemento */	
+// includes: Verifica se um certo elemento está presente no array 
+const inclui = arr.includes("Gandalf")
+console.log(inclui)
+
+// indexOf: Encontra o índice que possui o elemento indicado, ou -1 caso ele não exista no array
+const indice = arr.indexOf("Gandalf")
+console.log(indice)	
+
+/* Cortando e Concatenando*/
 	
+// slice: Copia uma parte do array e devolve a parte copiada sem alterar o array original
+const hobbits = arr.slice(0, 4)
+// Também pode ser usado com números negativos referenciar o final do array
+const outros = arr.slice(-4)
+console.log(arr)
+console.log(hobbits)
+console.log(outros)
+
+// concat: Junta dois ou mais arrays e/ou itens e devolve o resultado (sem alterar nenhum dos arrays)
+const sociedade = hobbits.concat(outros, "Boromir")
+console.log(sociedade)
+console.log(hobbits)
+console.log(outros)
 	
+/*Substituindo ELementos */
+	
+// splice: Permite remover elementos em qualquer posição do array e substituir por novos
+const elementosRemovidos = sociedade.splice(indice, 1, "Gandalf, o Cinzento")
+console.log(elementosRemovidos)
+console.log(sociedade)	
+	
+/*Iterando sobre os Elementos*/
+	
+// Usando o for para percorrer cada elemento do array
+for (let indice = 0; indice < sociedade.length; indice++) {
+  const elemento = sociedade[indice]
+  console.log(elemento + " se encontra na posição " + indice)
+}
