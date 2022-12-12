6e91e6b85b18ce21cc3750d3814eaecbaf061f27
Renato Anjo da Silva <103430282+RenatoAnjo@users.noreply.github.com>
2022-12-11 22:08:03 -0300

    Parametros de uma funcao continuacao

--- src/js/index.js
+++ src/js/index.js
@@ -4527,4 +4527,43 @@ function dizerOla(nome = "mundo") {
 dizerOla("Isaac")
 dizerOla()	
 	
+// Uma função pode ter quantos parâmetros quisermos, basta separaá-los por vírgula
+function soma(a, b) {
+  console.log(a + b)
+}
+soma(1, 1)
+soma(34, 5)
 	
+// Ao usar vários parâmetros, é recomendado deixar aqueles com valor padrão por último
+function criarUsuario(nome, email, senha, tipo = "leitor") {
+  const usuario = { nome, email, senha, tipo }
+  console.log(usuario)
+}
+
+function novoUsuario(nome, tipo = "leitor", email, senha) {
+  const usuario = { nome, email, senha, tipo }
+  console.log(usuario)
+}
+
+criarUsuario("Isaac", "isaac@email.com", "1234")
+novoUsuario("Isaac", "isaac@email.com", "1234")
+	
+// Ao criarmos uma função com muitos parâmetros,
+// uma boa prática é substituí-los por um objeto
+function parametrosDoJeitoErrado(nome, telefone, endereco, aniversario, email, senha) {
+  // ...
+}
+function parametrosDoJeitoCerto(usuario) {
+  // ...
+}
+// Além de facilitar na chamada da função, a ordem dos parâmetros se torna irrelevante
+parametrosDoJeitoErrado("nome", "telefone", "endereco", "aniversario", "email", "senha")
+const dadosDoUsuario = {
+  nome: "",
+  telefone: "",
+  email: "",
+  senha: "",
+  aniversario: "",
+  endereco: ""
+}
+parametrosDoJeitoCerto(dadosDoUsuario)	
