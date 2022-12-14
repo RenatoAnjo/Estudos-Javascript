d63585a85f82c61e1292a5e3493eb4a7773008cd
Renato Anjo da Silva <103430282+RenatoAnjo@users.noreply.github.com>
2022-12-13 22:32:51 -0300

    retorno de uma funcao, continuacao

--- src/js/index.js
+++ src/js/index.js
@@ -4590,4 +4590,36 @@ console.log(notebook)
 // Também podemos jogar a saída de uma função dentro dos parâmetros de outra
 console.log(criarProduto("Smartphone Samsung Galaxy", 1000))
 	
+// E o retorno não precisa ser uma variável,
+// pode ser o resultado de qualquer expressão
+function areaRetangular(base, altura) {
+  return base * altura
+}
+console.log(areaRetangular(3, 5))
+// Ou até o retorno de outra função
+function areaQuadrada(lado) {
+  return areaRetangular(lado, lado)
+}
+console.log(areaQuadrada(8))
 	
+// Uma função só pode ter uma saída, depois do return nada é executado
+// Repare que o VS Code nos avisa que nosso código não será executado
+function olaMundo() {
+  let texto = "..."
+  return texto
+  texto = "Olá, mundo!"
+  console.log(texto)
+}
+console.log(olaMundo())
+	
+// No entanto, diferentes ramificações dentro
+// da função podem ter diferentes retornos
+function maioridade(idade) {
+  if (idade >= 18) {
+    return "Maior de idade"
+  } else {
+    return "Menor de idade"
+  }
+}
+console.log(maioridade(20))
+console.log(maioridade(13))	
