3009b32e8728eca785e2f52926d36dbcb57a90dc
Renato Anjo da Silva <103430282+RenatoAnjo@users.noreply.github.com>
2022-12-29 22:16:00 -0300

    Sistema de Vagas de Emprego

--- src/js/index.js
+++ src/js/index.js
@@ -4868,3 +4868,35 @@ const racas = personagens.reduce(function (acumulador, personagem) {
 personagens.sort(function (a, b) {
   return a.nivel - b.nivel
 })
+	
+	/*Sistema de Vagas de Emprego*/
+	
+const vagas = []
+
+function listarVagas() {
+  const vagasEmTexto = vagas.reduce((textoFinal, vaga, indice) => {
+    textoFinal += indice + ". "
+    textoFinal += vaga.nome
+    textoFinal += " (" + vaga.candidatos.length + " candidatos)\n"
+    return textoFinal
+  }, "")
+
+  alert(vagasEmTexto)
+}
+	
+function novaVaga() {
+  const nome = prompt("Informe um nome para a vaga:")
+  const descricao = prompt("Informe um descrição para a vaga:")
+  const dataLimite = prompt("Informe um data limite (dd/mm/aaaa) para a vaga:")
+
+  const confirmacao = confirm(
+    "Deseja criar uma nova vaga com essas informações?\n" +
+    "Nome: " + nome + "\nDescrição: " + descricao + "\nData limite: " + dataLimite
+  )
+
+  if (confirmacao) {
+    const novaVaga = { nome, descricao, dataLimite, candidatos: [] }
+    vagas.push(novaVaga)
+    alert("Vaga criada.")
+  }
+}	
