c75fb34b6d7db2986dbf7c0357c45eca7277b96e
Renato Anjo da Silva <103430282+RenatoAnjo@users.noreply.github.com>
2023-01-06 20:48:35 -0300

    exercicio escalacao de times add jogador

--- src/js/index.js
+++ src/js/index.js
@@ -5044,3 +5044,24 @@ function removeContact() {
   contactSection.removeChild(titles[titles.length - 1])
   contactSection.removeChild(contacts[contacts.length - 1])
 }
+
+function addPlayer() {
+  const position = document.getElementById("position").value
+  const name = document.getElementById("name").value
+  const number = document.getElementById("number").value
+
+  const confirmation = cfirm("Escalar " + name + " como " + position + "?")
+
+  if (confirmation) {
+    const teamList = document.getElementById("team-list")
+    const playerItem = document.createElement("li")
+    playerItem.id = "player-" + number
+    playerItem.innerText = position + ": " + name + " (" + number + ")"
+    teamList.appendChild(playerItem)
+
+    document.getElementById("position").value = ""
+    document.getElementById("name").value = ""
+    document.getElementById("number").value = ""
+  }
+}	
+	
