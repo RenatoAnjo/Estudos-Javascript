98314bd4225ecd251cb4deca2b31848c761434f3
Renato Anjo da Silva <103430282+RenatoAnjo@users.noreply.github.com>
2023-01-14 21:13:21 -0300

    Arrow Functions

--- src/js/index.js
+++ src/js/index.js
@@ -5265,3 +5265,36 @@ document.getElementById('data').addEventListener('click', function () {
   input.dataset.somethingElse = 'Algum outro valor'
   console.log("O valor do atributo data-something-else agora é: " + input.dataset.somethingElse)
 })
+	
+/*Arrow Functions*/	
+function normalSum(a, b) {
+  return a + b
+}
+
+console.log(`Soma normal: ${normalSum(2, 2)}`)
+
+const anonymousSum = function (a, b) {
+  return a + b
+}
+
+console.log(`Soma anônima: ${anonymousSum(2, 2)}`)
+
+const arrowSum = (a, b) => {
+  return a + b
+}
+
+console.log(`Soma arrow function: ${arrowSum(2, 2)}`)
+
+const subtract = (a, b) => a - b
+
+console.log(`Subtração: ${subtract(5, 2)}`)
+
+const double = n => `O dobro de ${n} é ${n * 2}`
+
+const number = 21
+console.log(`Dobro: ${double(number)}`)
+
+const towns = ['Prontera', 'Izlude', 'Payon', 'Alberta', 'Geffen', 'Morroc']
+
+const startingWithP = towns.filter(town => town[0] === 'P')
+console.log(startingWithP)	
