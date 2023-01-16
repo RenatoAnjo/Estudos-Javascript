7b79fe29ac6e9398d0a3df6b8d0ae450285a274b
Renato Anjo da Silva <103430282+RenatoAnjo@users.noreply.github.com>
2023-01-15 22:58:05 -0300

    desestruturacao de arrays e objetos

--- src/js/index.js
+++ src/js/index.js
@@ -5298,3 +5298,35 @@ const towns = ['Prontera', 'Izlude', 'Payon', 'Alberta', 'Geffen', 'Morroc']
 
 const startingWithP = towns.filter(town => town[0] === 'P')
 console.log(startingWithP)	
+
+/*desestruturacao de arrays e objetos*/
+
+const person = {
+  name: "Luke",
+  job: "Farmer",
+  parents: ["Anakin", "Padme"]
+}
+
+const name = person.name
+
+const { job, parents } = person
+
+console.log(name, job, parents)
+
+const [father, mother] = parents
+
+console.log(father, mother)
+
+function createUser({ name, job, parents }) {
+  const id = Math.floor(Math.random() * 9999)
+  return {
+    id,
+    name,
+    job,
+    parents
+  }
+}
+
+const luke = createUser(person)
+
+console.log(luke)
