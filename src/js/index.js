faf8024f819f868db61854a458e509d916ae6d21
Renato Anjo da Silva <103430282+RenatoAnjo@users.noreply.github.com>
2023-01-21 23:45:54 -0300

    Encapsulamento continuacao

--- src/js/index.js
+++ src/js/index.js
@@ -5354,3 +5354,21 @@ class Account {
     return this.email === email && this.#password === password
   }
 }	
+
+/*Repare que fora da classe podemos acessar apenas o atributo email e
+ o método getBalance(), tentar acessar #password, 
+#balance ou #authenticate() gerará um erro:*/
+const user = {
+  email: "user@email.com",
+  password: "123456"
+}
+
+const userAccount = new Account(user)
+
+console.log(userAccount)
+console.log(userAccount.#password) // Gera erro
+console.log(userAccount.#balance) // Gera erro
+console.log(userAccount.#authenticate()) // Gera erro
+console.log(userAccount.getBalance("user@email.com", "123456"))
+console.log(userAccount.getBalance("user@email.com", "000000"))
+```
