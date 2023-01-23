ee32d4ccfbffb7413bae06ca246d4828c685fc82
Renato Anjo da Silva <103430282+RenatoAnjo@users.noreply.github.com>
2023-01-22 23:17:16 -0300

    Encadeamento Opcional

--- src/js/index.js
+++ src/js/index.js
@@ -5372,3 +5372,28 @@ console.log(userAccount.#authenticate()) // Gera erro
 console.log(userAccount.getBalance("user@email.com", "123456"))
 console.log(userAccount.getBalance("user@email.com", "000000"))
 ```
+/* Encadeamento Opcional*/
+
+const user = {
+  name: "John Doe",
+  email: "doejohn@email.com",
+  friends: [{
+    name: "Mary",
+    address: {
+      street: "Some Street",
+      number: 89
+    }
+  }],
+  age: 42,
+  phone: {
+    countryCode: "+55",
+    ddd: "22",
+    number: "998765432"
+  }
+}
+
+// console.log(user.friends[0].phone.ddd)
+console.log(user.friends[0].phone?.ddd)
+console.log(user?.brothers?.length)
+
+console.log(user.brothers?.[5].name)
