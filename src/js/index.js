270703d7a02b605a09004b1c2fa936666f8bfb54
Renato Anjo da Silva <103430282+RenatoAnjo@users.noreply.github.com>
2023-01-24 23:22:13 -0300

    CommonJS

--- src/js/index.js
+++ src/js/index.js
@@ -5411,4 +5411,45 @@ console.log({a, b})
 
 let c = false ?? 42
 
-console.log({c})
+console.log({c})]
+
+/*Módulos CommonJS*/
+
+function render() {
+  console.log('Renderizando a interface da aplicação...')
+}
+
+function store() {
+  console.log("Salvando as informações no banco de dados...")
+}
+
+console.log("Aplicação iniciada.")
+render()
+store()
+
+// render.js
+
+function render() {
+  console.log('Renderizando a interface da aplicação...')
+}
+
+module.exports = render
+
+// store.js
+
+function store() {
+  console.log("Salvando as informações no banco de dados...")
+}
+
+module.exports = store
+
+// index.js
+
+const render = require("./render")
+const store = require("./store")
+
+console.log("Aplicação iniciada.")
+render()
+store()
+console.log("Aplicação finalizada.")
+console.log("Aplicação finalizada.")
