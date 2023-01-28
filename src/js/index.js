3c698aff0dfd1b68c07eae881da897c61796d2c7
Renato Anjo da Silva <103430282+RenatoAnjo@users.noreply.github.com>
2023-01-27 23:54:25 -0300

    Promise

--- src/js/index.js
+++ src/js/index.js
@@ -5526,3 +5526,34 @@ function br() {
 console.log(label({ for: 'fullname', textContent: 'Nome Completo'}))
 console.log(input({ id: 'fullname', name: 'fullname', placeholder: 'Digite seu nome completo...'}))
 console.log(br())
+
+
+/*Promise*/
+function asyncSum(a, b) {
+    return new Promise((resolve, reject) => {
+        if (typeof a !== 'number' || typeof b !== 'number') {
+            reject('arguments must be of type number')
+        } else {
+            resolve(a + b)
+        }
+    })
+}
+
+function asyncSubtract(a, b) {
+    return new Promise((resolve, reject) => {
+        if (typeof a !== 'number' || typeof b !== 'number') {
+            reject('arguments must be of type number')
+        } else {
+            resolve(a - b)
+        }
+    })
+}
+
+const sumResult = asyncSum(50, 33)
+const subtractResult = asyncSubtract(50, null)
+
+Promise.all([sumResult, subtractResult]).then(results => {
+    console.log(results)
+}).catch(err => {
+    console.log(err)
+})
