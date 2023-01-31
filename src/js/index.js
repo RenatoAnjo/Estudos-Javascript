4188a873e220eb924c350ad2cd1dcb744ae4717b
Renato Anjo da Silva <103430282+RenatoAnjo@users.noreply.github.com>
2023-01-30 23:13:20 -0300

    tratamento de erro try, catch

--- src/js/index.js
+++ src/js/index.js
@@ -5597,3 +5597,28 @@ console.log(sum(undefined, 22))
 console.log(sum(18, "0"))
 console.log(sum(39, null))
 console.log(sum(13, "zero"))
+
+/*utilizar o bloco try para “tentar” executar uma parte do nosso código e 
+juntamente com ele um bloco catch para “capturar” os erros e executar um código em caso de erro:*/
+
+function sum(a, b) {
+    const firstNumber = Number(a)
+    const secondNumber = Number(b)
+
+    if (isNaN(firstNumber) || isNaN(secondNumber)) {
+        throw new Error('arguments must be two numbers')
+    }
+
+    return firstNumber + secondNumber
+}
+
+try {
+    console.log(sum(2, 9))
+    console.log(sum(true, 14))
+    console.log(sum(undefined, 22))
+    console.log(sum(18, "0"))
+    console.log(sum(39, null))
+    console.log(sum(13, "zero"))
+} catch {
+    console.log("An error ocurred!")
+}
