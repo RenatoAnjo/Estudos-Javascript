3a01459cdd7a863b3a5edc9d4067e2cc92db3635
Renato Anjo da Silva <103430282+RenatoAnjo@users.noreply.github.com>
2023-02-02 22:55:14 -0300

    Promise.all() com async e await

--- src/js/index.js
+++ src/js/index.js
@@ -5727,3 +5727,26 @@ function PhoneNumber(phoneNumberString) {
 
 console.log(new PhoneNumber('+55 (22) 9 9876-5432'))
 console.log(new PhoneNumber('+1 (555) a555-999-8888'))
+
+/*1. r funções async dentro do Promise.all(), afinal elas retornam uma promise. Porém, como o
+próprio Promise.all() retorna uma promise também podemos utilizar o await com ele. No código abaixo vemos 
+que a função .map() está retornando um array de promises pendentes:
+    
+Obs.: a função waitFor() é apenas uma forma simples de fazer o código esperar por uma quantidade
+arbitrária de tempo, assim simulamos um código que demora a ser executado.*/
+
+function waitFor(seconds) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve()
+    }, seconds * 1000)
+  })
+}
+
+const numbers = [4, 5, 9, 13, 77]
+
+const squares = numbers.map(async number => {
+    await waitFor(2)
+    return number * number
+})
+console.log(squares)
