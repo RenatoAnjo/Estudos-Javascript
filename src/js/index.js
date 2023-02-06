37c5e034594f5806be26bd6645706373030389d9
Renato Anjo da Silva <103430282+RenatoAnjo@users.noreply.github.com>
2023-02-05 22:15:44 -0300

    requisicoes POST

--- src/js/index.js
+++ src/js/index.js
@@ -5806,3 +5806,73 @@ async function getCountries() {
 }
 
 getCountries()
+
+/* o uso do fetch() para requisições GET como vimos na 
+aula anterior. No arquivo index.js iremos criar 
+uma função para obter os artigos salvos no db.json e
+outra para renderizá-los na tela, além de chamar a função 
+fetchArticles() assim que o documento estiver carregado*/
+
+function renderArticle(articleData) {
+  const article = document.createElement('article')
+  article.classList.add('article')
+  article.id = `article-${articleData.id}`
+
+  const title = document.createElement('h3')
+  title.classList.add('article-title')
+  title.textContent = articleData.title
+
+  const content = document.createElement('div')
+  content.classList.add('article-content')
+  content.innerHTML = articleData.content
+
+  const author = document.createElement('div')
+  author.classList.add('article-author')
+  author.textContent = articleData.author
+
+  article.append(title, author, content)
+  document.querySelector('#articles').appendChild(article)
+}
+
+async function fetchArticles() {
+  const articles = await fetch("http://localhost:3000/articles").then(res => res.json())
+  articles.forEach(renderArticle)
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  fetchArticles()
+})
+
+
+/*Repare que se adicionarmos uma action e um method ao
+nosso form ele já consegue fazer requisições para o json-server,
+criando um novo artigo, e como a página é atualizada o novo artigo
+já aparece na tela. No entando, o que queremos é fazer as requisições
+POST através do javascript sem atualizar a página. Para isso podemos 
+usar o fetch() novamente, dessa vez com algumas opções a mais*/
+
+const form = document.querySelector('form')
+
+form.addEventListener('submit', async (ev) => {
+  ev.preventDefault()
+
+  const articleData = {
+    title: document.querySelector('#title').value,
+    author: document.querySelector('#author').value,
+    content: document.querySelector('#content').value
+  }
+
+  const response = await fetch('http://localhost:3000/articles', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(articleData)
+  })
+
+  const savedArticle = await response.json()
+  form.reset()
+  renderArticle(savedArticle)
+
+  console.log(savedArticle)
+})
