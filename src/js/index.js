24f2f2f76efc27455b7c7696cef940e82f7d9897
Renato Anjo da Silva <103430282+RenatoAnjo@users.noreply.github.com>
2023-02-07 22:01:23 -0300

    Refatorando as Promises

--- src/js/index.js
+++ src/js/index.js
@@ -5943,3 +5943,43 @@ form.addEventListener('submit', async (ev) => {
 
   console.log(savedArticle)
 })
+
+/*calcula o imc*/
+
+async function imc(weight, height) {
+  if (typeof weight  !== 'number' || typeof height !== 'number') {
+    return Promise.reject('arguments must be of type number')
+  }
+  return weight / (height * height)
+}
+
+
+/*fazendo uso do await e dos blocos try e catch:*/
+
+// ...
+
+async function showImc(weight, height) {
+  try {
+    console.log(`Calculando o IMC para peso ${weight} e altura ${height}...`)
+
+    const result = await imc(weight, height)
+    
+    console.log(`O resultado do IMC foi de ${result}.`)
+  
+    if (result < 18.5) console.log('Situação: MAGREZA')
+    else if (result < 25) console.log('Situação: NORMAL')
+    else if (result < 30) console.log('Situação: SOBREPESO')
+    else if (result < 40) console.log('Situação: OBESIDADE')
+    else console.log('Situação: OBESIDADE GRAVE')
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// ...
+
+showImc(71, 1.74)
+showImc(48, 1.60)
+showImc(71, "texto")
+showImc(82, 1.72)
+showImc(120, 1.80)
